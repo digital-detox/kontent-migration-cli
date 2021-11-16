58ade055c5703dd841ebd09687e06c0399ba2a24
Alex Canessa <canessa.alex@gmail.com>
2021-11-16 16:06:32 +0100

    feat: improve error messaging

--- src/lib/run.ts
+++ src/lib/run.ts
@@ -132,14 +132,14 @@ export default async () => {
 
       migrationTask.succeed();
     } catch (error) {
-      const validationErrorsStr = error.validationErrors.reduce(
-        (prevErr, err) => {
+      const validationErrorsStr = (error.validationErrors || []).reduce(
+        (prevErr: string, err: Error) => {
           return `${prevErr}${err.message}\n`;
         },
         "\nValidation Errors:\n"
       );
       migrationTask.fail(
-        `The migration ${description} failed because ... ${error.message}${validationErrorsStr}`
+        `The migration ${description} failed for the following reason:\n${error.message}${validationErrorsStr}`
       );
       process.exit(0);
     }
